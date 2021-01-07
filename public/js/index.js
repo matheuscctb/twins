be57d222734e545cbef25991f59913acfd14cc80
Sara Santos Satana <bimiotgetec@gmail.com>
2021-01-07 08:24:56 -0300

    make it better

---
+++ public/js/index.js
@@ -0,0 +1,250 @@
+var viewer1;
+var viewer;
+
+function showViewer() {
+
+
+    var options = {
+        env: 'AutodeskProduction',
+        api: 'derivativeV2', // TODO: for models uploaded to EMEA change this option to 'derivativeV2_EU'
+        getAccessToken: getForgeToken
+    };
+    // var options = {
+    //     env: 'AutodeskProduction',
+    //     getAccessToken: getForgeToken
+    // };
+
+    Autodesk.Viewing.Initializer(options, function() {
+
+        var obra = document.getElementById('obras').value;
+        if (obra == "Duo Salvador Norte") {
+            //var documentId1 = 'urn:dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6d2djaWloemhodW11Z2J6Z29jc2dhaXZ0ZWpjbXZldHotcjIvUHIlQzMlQTlkaW8lMjBDT1JSSUdJRE8lMjAzJTIwKDMpLnJ2dA==';
+            //dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6d2djaWloemhodW11Z2J6Z29jc2dhaXZ0ZWpjbXZldHotc25yL1NOUl9IQUJfRklYXzAyXzIwMTlfUjEucnZ0
+
+            var documentId1 = 'urn:dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6d2djaWloemhodW11Z2J6Z29jc2dhaXZ0ZWpjbXZldHotcjIvUHIlQzMlQTlkaW8lMjBDT1JSSUdJRE8lMjAzJTIwKDMpLnJ2dA==';
+
+            //var documentId = 'urn:dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6d2djaWloemhodW11Z2J6Z29jc2dhaXZ0ZWpjbXZldHotcjIvUHIlQzMlQTlkaW8lMjBDT1JSSUdJRE8lMjAzJTIwKDMpLnJ2dA==';
+            //dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6d2djaWloemhodW11Z2J6Z29jc2dhaXZ0ZWpjbXZldHotc25yL1NOUl9IQUJfRklYXzAyXzIwMTlfUjEucnZ0
+
+            var documentId = 'urn:dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6d2djaWloemhodW11Z2J6Z29jc2dhaXZ0ZWpjbXZldHotcjIvUHIlQzMlQTlkaW8lMjBDT1JSSUdJRE8lMjAzJTIwKDMpLnJ2dA==';
+        } else {
+            var documentId = 'urn:dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6d2djaWloemhodW11Z2J6Z29jc2dhaXZ0ZWpjbXZldHotc25yL1NOUl9IQUJfRklYXzAyXzIwMTlfUjEucnZ0';
+            var documentId1 = 'urn:dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6d2djaWloemhodW11Z2J6Z29jc2dhaXZ0ZWpjbXZldHotc25yL1NOUl9IQUJfRklYXzAyXzIwMTlfUjEucnZ0';
+
+        }
+
+
+
+        viewer1 = new Autodesk.Viewing.GuiViewer3D(document.getElementById('forgeViewer1'), {
+            extensions: ['PanelInfoViewerExtension']
+        });
+        viewer1.start();
+
+        Autodesk.Viewing.Document.load(documentId1, onDocumentLoadSuccess1, onDocumentLoadFailure1);
+
+
+
+        viewer = new Autodesk.Viewing.GuiViewer3D(document.getElementById('forgeViewer'), {
+            extensions: ['PanelInfoViewerExtension', 'ManutencaoViewerExtension']
+        });
+        viewer.start();
+
+
+        //var documentId = 'urn:' + urn;
+        Autodesk.Viewing.Document.load(documentId, onDocumentLoadSuccess, onDocumentLoadFailure);
+    });
+
+    function onDocumentLoadSuccess1(doc) {
+        console.log("Passou por sucesso")
+            // if a viewableId was specified, load that view, otherwise the default view
+        var viewables1 = doc.getRoot().getDefaultGeometry();
+        viewer1.loadDocumentNode(doc, viewables1).then(i => {
+            // any additional action here?
+            //console.log("Passou por sucesso 2")
+        });
+    }
+
+    function onDocumentLoadFailure1(viewerErrorCode) {
+        console.log("Passou por falha")
+        console.error('onDocumentLoadFailure() - errorCode:' + viewerErrorCode);
+    }
+
+
+    function onDocumentLoadSuccess(doc) {
+        var viewables = doc.getRoot().getDefaultGeometry();
+        viewer.loadDocumentNode(doc, viewables).then(i => {
+            // documented loaded, any action?
+
+            var obra = document.getElementById('obras').value;
+            //var obra = "Duo Salvador Norte - Teste"
+            var bloco = document.getElementById('bloco').value;
+            var vetorBlocos = []
+            firebase.database().ref('obras/' + obra + '/sequenciareal/' + bloco).once('value').then(snapshot => {
+                var todosBlocos = (Object.keys(snapshot.val()));
+                todosBlocos.forEach(value => {
+                    // snapshot.val()[value].status
+                    vetorBlocos.push({
+                        bloco: value,
+                        status: snapshot.val()[value].status
+                    })
+                })
+                var vetorTodosElementos = []
+                getAllLeafComponents(viewer, (dbIds) => {
+                    //Propriedade que quero buscar das elemetos dos setores e formas
+                    const filteredProps = ['Numeração', "Agrupamento"];
+                    // Get only the properties we need for the leaf dbIds
+                    this.viewer.model.getBulkProperties(dbIds, filteredProps, (items) => {
+                        // Iterate through the elements we found
+                        items.forEach((item) => {
+                            // and iterate through each propertys
+                            item.properties.forEach(function(prop) {
+                                vetorTodosElementos.push(item);
+                            });
+                        });
+                        //Removendo elementos duplicados
+                        var elementos = [...new Set(vetorTodosElementos)];
+
+                        var isolated = []
+                        var aux = 0;
+
+                        console.log('vetor blocos: ', vetorBlocos)
+                        console.log('elemtnso:  ', elementos)
+                        for (i = vetorBlocos.length - 1; i >= 0; i--) {
+                            //Identifica o primeiro pavimento com forma
+                            if (vetorBlocos[i].status == 1 && aux == 0) {
+                                elementos.forEach(value => {
+                                    //console.log("valur: ", value)
+
+                                    if ('0' + value.properties[1].displayValue == vetorBlocos[i].bloco ||
+                                        value.properties[1].displayValue == 0) {
+
+                                        console.log(`value.properties[1].displayValue= ${value.properties[1].displayValue}   vetorBlocos[i].bloco:  ${vetorBlocos[i].bloco}    i: ${i}  `)
+                                        console.log("aqui1")
+                                        isolated.push(value.dbId);
+                                    }
+                                })
+                                aux = 1;
+                            }
+                            //pavimentos anteriores sem formas
+                            else if (vetorBlocos[i].status == 1 && aux == 1) {
+                                console.log(vetorBlocos[i].bloco)
+                                elementos.forEach(value => {
+                                    if ((!value.properties[0].displayValue) && '0' + value.properties[1].displayValue == vetorBlocos[i].bloco) {
+                                        console.log("aqui2")
+                                        isolated.push(value.dbId);
+                                    }
+                                })
+                            }
+                            //obra só com estrutura do piso
+                            else if (vetorBlocos[i].status == 0 && vetorBlocos[i].bloco == vetorBlocos[0].bloco) {
+                                elementos.forEach(value => {
+                                    if (value.properties[1].displayValue == 0) {
+                                        console.log("aqui3")
+                                        isolated.push(value.dbId);
+                                    }
+                                })
+                            }
+                        }
+                        viewer.isolate(isolated)
+                        viewer.setGhosting(false);
+                    });
+                });
+
+            });
+        });
+        // NestedViewerExtension
+        viewer.loadExtension("NestedViewerExtension", {
+                filter: ["2d", "3d"],
+                crossSelection: true
+            })
+            // NestedViewerExtension
+    }
+
+    function onDocumentLoadFailure(viewerErrorCode) {
+        console.error('onDocumentLoadFailure() - errorCode:' + viewerErrorCode);
+    }
+
+    function getForgeToken(callback) {
+        fetch('/api/forge/oauth/token').then(res => {
+            res.json().then(data => {
+                callback(data.access_token, data.expires_in);
+            });
+        });
+    }
+}
+
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyCk_aoscgy9uwGQJ3OfEcBulfttjH2Mzxw",
+    authDomain: "carol-56175.firebaseapp.com",
+    databaseURL: "https://carol-56175.firebaseio.com",
+    projectId: "carol-56175",
+    storageBucket: "carol-56175.appspot.com",
+    messagingSenderId: "497363212618",
+    appId: "1:497363212618:web:39701f8718c372bcbc09b3",
+    measurementId: "G-9TE9525H3C"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+// firebase.analytics();
+
+firebase.database().ref('obras/').once('value', function(snapshot) {
+    var i = 0;
+    snapshot.forEach(function(item) {
+        var select = document.getElementById("obras");
+        var options = document.createElement("option");
+        opt_txt = document.createTextNode(item.key);
+        options.appendChild(opt_txt);
+        options.setAttribute("value", item.key);
+        select.appendChild(options);
+        i++;
+    });
+    $("#obras").val($("#obras option:first").val());
+    var obra = document.getElementById('obras').value;
+    //var obra = "Duo Salvador Norte - Teste"
+    console.log(obra)
+    firebase.database().ref('obras/' + obra + '/sequenciareal').once('value', function(snapshot) {
+
+        console.log('snaptsho.val: ', snapshot.val())
+        var i = 0;
+        snapshot.forEach(function(item) {
+            var select = document.getElementById("bloco");
+            var options = document.createElement("option");
+            opt_txt = document.createTextNode(item.key);
+            options.appendChild(opt_txt);
+            options.setAttribute("value", item.key);
+            select.appendChild(options);
+            i++;
+        });
+        $("#bloco").val($("#bloco option:first").val());
+    });
+})
+
+
+$('#obras').on('change', function() {
+    var obra = document.getElementById('obras').value;
+    //var obra = "Duo Salvador Norte - Teste"
+    $("#bloco").empty();
+    firebase.database().ref('obras/' + obra + '/sequenciareal').once('value', function(snapshot) {
+        var i = 0;
+        snapshot.forEach(function(item) {
+            var select = document.getElementById("bloco");
+            var options = document.createElement("option");
+            opt_txt = document.createTextNode(item.key);
+            options.appendChild(opt_txt);
+            options.setAttribute("value", item.key);
+            select.appendChild(options);
+            i++;
+        });
+        $("#bloco").val($("#bloco option:first").val());
+
+    });
+
+    $('[apagar="forge"]').html(`<div role="tabpanel" class="tab-pane fade in active" id="forgeViewer" class="col-xs-12 col-md-8"></div>
+    <div role="tabpanel" class="tab-pane fade in active" id="forgeViewer1" class="col-xs-12 col-md-8"></div>`)
+    showViewer();
+
+});
+
+showViewer();
